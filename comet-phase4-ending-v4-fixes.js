7d13d66d510f85d43161ff4ec91b5db74162c847
azyuen <azyuen@gmail.com>
2026-09-17 00:59:40 +1000

    Add direct PHS4 ending preview shortcut

--- comet-phase4-ending-v4-fixes.js
+++ comet-phase4-ending-v4-fixes.js
@@ -1,7 +1,8 @@
 // Small state guards for Phase 4 ending v4.
 (() => {
   if(!window.CometPhase4EndingV4||typeof GameScene==='undefined')return;
-  const proto=GameScene.prototype;
+  const proto=GameScene.prototype,P4=window.CometPhase4;
+  const baseShowPhase4SystemBirth=proto.showPhase4SystemBirth;
   const baseStartUniverseFinale=proto.startUniverseFinale;
   const baseShowJoinedSuperclusterResult=proto.showJoinedSuperclusterResult;
   const baseCompleteUniverseAssembly=proto.completeUniverseAssembly;
@@ -10,6 +11,26 @@
 
   function addLabReturn(scene){
     if(scene._labSandboxRun&&typeof scene.returnFromLabPhase==='function')scene.miniButton(49,scene.Y(18),76,24,'LAB',C.purple,()=>scene.returnFromLabPhase());
+  }
+
+  if(typeof baseShowPhase4SystemBirth==='function'){
+    proto.showPhase4SystemBirth=function(){
+      const result=baseShowPhase4SystemBirth.call(this);
+      if(this._labSandboxRun&&this._labSandboxPhase==='PHS4'){
+        addLabReturn(this);
+        this.miniButton(W-49,this.Y(18),76,24,'ENDING',C.orange,()=>{
+          this.tierIndex=P4.superclusterTier;
+          this.growth=0;
+          this.finaleMergeCount=0;
+          this._finalJoinedSuperclusters=[];
+          this._phase4EpilogueUnlocked=false;
+          this._phase4EpilogueStarted=false;
+          this.setPlayer(true);
+          return this.showPhaseCompleteCard(4);
+        });
+      }
+      return result;
+    };
   }
 
   proto.startUniverseFinale=function(){
@@ -53,5 +74,5 @@
     return baseContinueNextUniverse.call(this);
   };
 
-  window.CometPhase4EndingV4Fixes=Object.freeze({enabled:true,cleanEpilogueStart:true,labExitThroughoutEpilogue:true,gameCompleteEndsRun:true,continueStartsRun:true});
+  window.CometPhase4EndingV4Fixes=Object.freeze({enabled:true,cleanEpilogueStart:true,phs4EndingShortcut:true,labExitThroughoutEpilogue:true,gameCompleteEndsRun:true,continueStartsRun:true});
 })();
