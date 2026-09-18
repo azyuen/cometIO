78f54ba7da342ae0c8947ef33ef85290e8ae67d0
azyuen <azyuen@gmail.com>
2026-09-18 16:34:52 +1000

    Bump universe sprite asset version

--- comet-cosmic-sprite-assets-v1.js
+++ comet-cosmic-sprite-assets-v1.js
@@ -23,10 +23,10 @@
   COMET_SPRITE_ASSETS.universe_final = {
     family: 'universe',
     lods: [32, 64],
-    version: 1,
+    version: 2,
     files: {
-      32: 'assets/sprites/universe/universe_final_32.png?v=1',
-      64: 'assets/sprites/universe/universe_final_64.png?v=1'
+      32: 'assets/sprites/universe/universe_final_32.png?v=2',
+      64: 'assets/sprites/universe/universe_final_64.png?v=2'
     }
   };
 
